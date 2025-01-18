5cff0c95847ac40c85350eee4cb84750655e1100
RCoskun <ridvancoskun1688@gmail.com>
2025-01-18 15:49:49 +0300

    Add Modal Yapildi

---
+++ RuntimeEfCoreWeb/Controllers/UdtController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RuntimeEfCoreWeb.Controllers
+{
+
+
+    [Route("Udt/{entityName}")]
+    public class UdtController : Controller
+    {
+        public UdtController(IConfiguration configuration)
+        {
+            DynamicContextExtensions.DynamicContext(configuration.GetConnectionString("DefaultConnection")!);
+        }
+        public IActionResult Index(string entityName)
+        {
+
+            var items = DynamicContextExtensions.GetEntity(entityName);
+            return View(items);
+        }
+    }
+}
